792f3a9636ccba86ed93763cbae2a2b14936cfd4
yeonjaedev <yjae47@gmail.com>
2022-10-06 19:34:42 +0900

    [ADD] user service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { HttpException, Injectable, UnauthorizedException } from '@nestjs/common';
+import { BadRequestException, HttpException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Users } from 'src/entities/Users';
 import { Repository } from 'typeorm';
@@ -11,13 +11,13 @@ export class UsersService {
     ) {}
     async postUsers(email: string, nickname: string, password:string)  {
         if(!email) {
-            throw new HttpException('이메일이 없습니다.',400);
+            throw new BadRequestException('이메일이 없습니다.');
         }
         if(!nickname) {
-            throw new HttpException('닉네임이 없습니다.',400);
+            throw new BadRequestException('닉네임이 없습니다.');
         }
         if(!password) {
-            throw new HttpException('비밀번호가 없습니다.',400);
+            throw new BadRequestException('비밀번호가 없습니다.');
         }
         const user = await this.usersRepository.findOne({where:{email}});
         if( user ){
